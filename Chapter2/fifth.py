df4013e4621ff164992f016b90be2807d09743c1
Anamika <anamikasharma16649@gmail.com>
2026-05-09 10:42:16 +0530

    new file:   Chapter2/fifth.py and modified:   Chapter2/fourth.py and new file:   practiceset2/practice1.py

---
+++ Chapter2/fifth.py
@@ -0,0 +1,29 @@
+# input function
+
+
+a = input("Enter name:")
+print("name is: ", a)
+
+
+p = input("Enter the number1: ")
+q = input("Enter the number2: ")
+
+print("number1 is:", p)
+print("number2 is:", q)
+
+print("The sum of number1 and number2 is:", p+q)  #  ye values combile ho jayegi plus nhi hogi kyuki jo bhi humne input 
+#di hoti hai cahhe no. ho ya string uski output hmesaha string mani jati hai to jb bhi hum plus ka sign lgate hai to 
+#output concatenate jud jati hai kyuki v string mani jati hai
+# to iske liyr hume input ko jo humne input di hoti hai usse int mai cinvert krna pdta hai(type conversion)  string to int
+
+
+
+
+r = int(input("Enter the number1: "))
+k = int(input("Enter the number2: "))
+
+print("number1 is:", r)
+print("number2 is:", k)
+
+print("The sum of number1 and number2 is:", r+k)
+
--- Chapter2/fourth.py
+++ Chapter2/fourth.py
@@ -1,7 +1,27 @@
 a = 31
 print(type(a))
 
+print(str(a))
 
 
-print(str(51))
-# print(b)
+
+b = "annu"
+print(type(b))
+
+c = 51
+print(float(c))
+
+
+d = 23.8
+print(int(d))
+
+
+
+
+e = " 44.6"
+print(type(e))
+print(float(e))   # e but the type should be float but it is string so we have to convert it into float first and then we can use it as a float number
+
+
+
+
