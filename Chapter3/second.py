ef485ba6e50a14a9d2236d96bf1f684f9b49d091
Anamika <anamikasharma16649@gmail.com>
2026-05-10 12:42:15 +0530

    modified:   Chapter3/second.py and new file:   Chapter3/third.py

--- Chapter3/second.py
+++ Chapter3/second.py
@@ -10,3 +10,11 @@ print(name[-5:-2])
 print(name[0:3])
 print(name[:3])       #yha staring mai kuch ni likha mtlb o se start hai
 print(name[2:])   #yha ending mai kuch ni likha mtlb length hai iska mtlb
+
+
+
+# slicing with skip
+
+
+a = "abcdefghijklmnopqrstuvwxyz"
+print([a[1:8:3]])
---
+++ Chapter3/third.py
@@ -0,0 +1,23 @@
+# length of string
+
+name = "anamika sharma"
+print(len(name))
+
+
+# string endswith
+
+print(name.endswith("ma"))
+print(name.endswith("am"))
+
+#  string startswith
+
+print(name.startswith("anam"))
+
+# count krti hai hum koi bhi word bracets mai dalege vo uss word ki occurance ko count kregi
+
+print(name.count("a"))
+
+
+# string capatalization
+
+print(name.capitalize())
