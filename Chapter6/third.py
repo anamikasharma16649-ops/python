f27f1c9db36ecf4cf08eb8ae140dc2158a6f97a3
Anamika <anamikasharma16649@gmail.com>
2026-05-12 06:20:48 +0530

    new file:   Chapter6/first.py, new file:   Chapter6/second.py, new file:   Chapter6/third.py

---
+++ Chapter6/third.py
@@ -0,0 +1,10 @@
+#  w.a.p to print yes when the age entered by the user is graeter than or equal to 18.
+
+age = int(input("Enter your age: "))
+
+if(age>=18):
+    print("Yes")
+
+else:
+    print("No")
+    
