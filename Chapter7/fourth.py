16dad2a906efd683fc9db7db85477873cd08e3d7
Anamika <anamikasharma16649@gmail.com>
2026-05-14 21:17:51 +0530

    Update commit message

--- Chapter7/fourth.py
+++ Chapter7/fourth.py
@@ -18,3 +18,34 @@ for i in range(1,21):
 n = int(input("Enter number: "))
 for i in range(1,11):
     print(f"{n} * {i} = {n*i}")
+
+# 1 se 10 tak numbers print karo using while loop
+
+
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+# 1 se 20 tak sirf odd numbers print karo using while loop
+i = 1
+while i <= 20:
+    if i % 2 != 0:
+        print(i)
+    i += 1
+
+
+# 👉 Reverse counting print karo: 10 to 1 using while loop
+
+
+i = 10
+while i >= 1:
+    print(i)
+    i -= 1
+
+
+fact = 1
+a = int(input("Enter number: "))
+for i in range(1, a+1):
+    fact = fact*i
+print(f"The factorial is: {fact}")
