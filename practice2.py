42f790f238ac185378d230645712da660d25a312
Anamika <anamikasharma16649@gmail.com>
2026-05-15 06:56:08 +0530

    update commit message

--- practice2.py
+++ practice2.py
@@ -5,3 +5,38 @@ def f_to_c(f):
 f = int(input("Enter tempreature in fahrenheit: "))
 c = f_to_c(f)
 print("Temperature in Celsius:", c)
+
+
+
+
+
+
+
+
+
+
+
+
+##########deleted# 
+
+class Student:
+    name = "Anamika"
+    age = 19 
+    marks = 100
+    def info(self):
+        print(f"{self.name} is a student of class 12th and scored {self.marks} marks")
+
+s = Student()
+print(s.name)
+s.info()
+
+
+class Person:
+    def __init__(self, name, occ):
+        self.name = name
+        self.occ = occ
+    def greet(self):
+        print(f"{self.name} is a {self.occ}")
+
+p = Person("Anamika", "software developer")
+p.greet()
